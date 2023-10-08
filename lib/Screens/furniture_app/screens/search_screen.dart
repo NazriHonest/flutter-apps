ea5848bdb5cf137f1006a7abc40a576b0b6d6fc2
NazriHonest <naazir21@gmail.com>
2023-10-08 17:18:13 +0100

    first commit

--- lib/Screens/furniture_app/screens/search_screen.dart
+++ lib/Screens/furniture_app/screens/search_screen.dart
@@ -6,38 +6,31 @@ class SearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Search an Item")),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          children: [
-            const SizedBox(height: 20),
-            Container(
-              alignment: Alignment.center,
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              width: double.infinity,
-              height: 45,
-              decoration: BoxDecoration(
-                color: Colors.grey.shade200,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: const TextField(
-                decoration: InputDecoration(
-                  hintText: "Search here",
-                  enabledBorder: InputBorder.none,
-                  focusedBorder: InputBorder.none
-                ),
-              )
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: (){}, 
-              child: const Text("Search")
-            ),
-          ],
-        ),
-      )
-    );
-    
+        appBar: AppBar(title: const Text("Search an Item")),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  width: double.infinity,
+                  height: 45,
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade200,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                        hintText: "Search here",
+                        enabledBorder: InputBorder.none,
+                        focusedBorder: InputBorder.none),
+                  )),
+              const SizedBox(height: 20),
+              ElevatedButton(onPressed: () {}, child: const Text("Search")),
+            ],
+          ),
+        ));
   }
 }
